a5a47eaa71b0198bcf770d9009e28b848b6dcfe4
manou <aitameurimene23@gmail.com>
2024-04-23 21:19:35 +0200

    correction28

--- app/src/main/java/com/simplecity/amp_library/ui/modelviews/AlbumView.java
+++ app/src/main/java/com/simplecity/amp_library/ui/modelviews/AlbumView.java
@@ -128,11 +128,11 @@ public class AlbumView extends MultiItemView<AlbumView.ViewHolder, Album> implem
         }
 
         requestManager.load(album)
-                .listener(getViewType() == ViewType.ALBUM_PALETTE ? GlidePalette.with(album.getArtworkKey())
-                        .use(GlidePalette.Profile.MUTED_DARK)
-                        .intoBackground(holder.bottomContainer)
-                        .crossfade(true)
-                        : null)
+                listener(getViewType() == ViewType.ALBUM_PALETTE ? GlidePalette.with(album.getArtworkKey())
+                .use(BitmapPalette.Profile.MUTED_DARK) // Use static access with fully qualified class name
+                .intoBackground(holder.bottomContainer)
+                .crossfade(true)
+                : null)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(PlaceholderProvider.getInstance(holder.itemView.getContext()).getPlaceHolderDrawable(album.name, false, settingsManager))
                 .into(holder.imageOne);
